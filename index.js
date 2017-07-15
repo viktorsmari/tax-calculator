b54011f17bcc83152ef18ec5af142cadcaba57da
Viktor Smari <viktorsmari@gmail.com>
2017-07-15 17:16:24 +0200

    Update for year 2017, one less tax step

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ var myapp = new Vue({
   el: '#app',
   data: {
     totalSalary:336035,
-    personalDiscountYear:623042,
+    personalDiscountYear:634880,
     steps: [
-      { taxPct: 37.13, limit: 336035},
-      { taxPct: 38.35, limit: 836990},
-      { taxPct: 46.25, limit: 0},
+      { taxPct: 36.94, limit: 834707},
+      { taxPct: 46.25, limit: 0}
     ],
 
     examples: [
@@ -14,7 +13,7 @@ var myapp = new Vue({
       { price: 300000},
       { price: 350000},
       { price: 400000},
-      { price: 650000},
+      { price: 834707},
       { price: 900000},
       { price: 1200000},
     ]
@@ -32,31 +31,16 @@ var myapp = new Vue({
       }
     },
     taxFromStep2: function(){
-      // af ollu fra 336.036 til 836.990
-      if (this.totalSalary > this.steps[0].limit) {
-
-        //if upphaed er meira en 836, borga allt skattthrepid
-        if (this.totalSalary < this.steps[1].limit){
-        // borga skatt2 af upphaed a thessu bili
-           return Math.round(this.steps[1].taxPct / 100 * (this.totalSalary - this.steps[0].limit));
-        } else {
-          return Math.round(this.steps[1].taxPct / 100 * (this.steps[1].limit - this.steps[0].limit));
-        }
-      } else {
-        return 0;
-      }
-    },
-    taxFromStep3: function(){
       // af ollu yfir 836.990
-      if (this.totalSalary > this.steps[1].limit) {
-        return Math.round(this.steps[2].taxPct / 100 * (this.totalSalary - this.steps[1].limit));
+      if (this.totalSalary > this.steps[0].limit) {
+        return Math.round(this.steps[1].taxPct / 100 * (this.totalSalary - this.steps[0].limit));
       } else {
         return 0;
       }
     },
     totalTax: function(){
       // All tax steps combined - personal tax discount
-      return this.taxFromStep1 + this.taxFromStep2 + this.taxFromStep3 - this.personalMonth;
+      return this.taxFromStep1 + this.taxFromStep2 - this.personalMonth;
     },
     totalLeft: function(){
       return this.totalSalary - this.totalTax;
